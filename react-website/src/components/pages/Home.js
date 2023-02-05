1cca033702fc0ace821ab4ab361500ea290da5d1
tararoshan <troshan@utexas.edu>
2023-02-04 18:53:48 -0600

    Update font, background color

--- react-website/src/components/pages/Home.js
+++ react-website/src/components/pages/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
                         <li style={{marginBottom:"0.5em"}}>70% of women take some form of maternity leave</li>
                         <li style={{marginBottom:"0.5em"}}>Generally, women drop out/slow their career due to being primary caregivers</li>
                     </ul>
-                    <h2 style={{fontSize:"2em"}}>&nbsp;We understand, and we're here to help.</h2>
+                    {/* <h2 style={{fontSize:"2em"}}>&nbsp;We understand, and we're here to help.</h2> */}
                     <h2 style={{fontSize:"1.5em"}}>&nbsp;&nbsp;Head to the 'Start' tab to get started with WiCE.</h2>
                     <hr size="20" ></hr>
                 <Feedback/>
